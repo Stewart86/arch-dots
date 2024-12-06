403f88252e9169d546291ed4b763e7cea0415e33
stewart86 <siwei.wong@gmail.com>
2024-12-07 00:22:07 +0800

    ♻️ refactor(structure): move common scripts to .local/bin

---
+++ .local/bin/updates.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define threshholds for color indicators
+threshhold_green=0
+threshhold_yellow=25
+threshhold_red=100
+
+# Calculate available updates
+updates=0
+updates=$((updates + "$(checkupdates 2>/dev/null | wc -l || 0)"))
+updates=$((updates + "$(paru -Qu --aur --quiet | wc -l || 0)"))
+updates=$((updates + "$(flatpak remote-ls --updates 2>/dev/null | wc -l || 0)"))
+
+# Output in JSON format for Waybar Module custom-updates
+css_class="green"
+
+if [ "$updates" -gt $threshhold_yellow ]; then
+    css_class="yellow"
+fi
+
+if [ "$updates" -gt $threshhold_red ]; then
+    css_class="red"
+fi
+
+if [ "$updates" -gt $threshhold_green ]; then
+    printf '{"text": "%s", "alt": "%s", "tooltip": "Click to update your system", "class": "%s"}' "$updates" "$updates" "$css_class"
+else
+    printf '{"text": "0", "alt": "0", "tooltip": "No updates available", "class": "green"}'
+fi
