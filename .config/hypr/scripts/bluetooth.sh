32b7a415e327ca36cfd215d173ec07f3e67a0e1c
stewart86 <siwei.wong@gmail.com>
2024-12-25 21:12:25 +0800

    ✨ feat(uwsm): enhance uwsm config and systemd app management

--- .config/hypr/scripts/bluetooth.sh
+++ .config/hypr/scripts/bluetooth.sh
@@ -4,7 +4,6 @@ while :; do
   if [ "$(rfkill -n | rg "bluetooth" | awk '{print $4}')" == "unblocked" ]; then
     break
   fi
-  sleep 1
   notify-send "Bluetooth" "Unblocking Bluetooth"
   rfkill unblock bluetooth
 done
